4ba5d4011f8a5a4b0de7f26079075c26cb60e359
webbe32 <webb.edwin@gmail.com>
2022-04-21 14:19:55 -0400

    6-7

--- hw1.js
+++ hw1.js
@@ -1,3 +1,11 @@
-let navbar            = getElementById('navbar'); 
-let variable_showcase = getElementById('showcase');
-let footer            = getElementById('footer'); 
+let navbar            = document.getElementById('navbar'); 
+let variable_showcase = document.getElementById('showcase');
+let footer            = document.getElementById('footer'); 
+
+// let footerNodes = document.querySelectorAll("footer"); 
+let footerNodes = footer.childNodes;
+console.log(footerNodes);
+
+// let footerChildren = document.getElementsByTagName("footer"); 
+let footerChildren = footer.children; 
+console.log(footerChildren); 
