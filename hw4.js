d229f23a797fd0bd6180f2516ef7612c0928798d
webbe32 <webb.edwin@gmail.com>
2022-04-22 15:51:15 -0400

    hw4-step16

--- hw4.js
+++ hw4.js
@@ -14,4 +14,27 @@ console.log(upperCase);
 //returns HELLO WORLD
 
 console.log(str1AllCaps[1]); 
-console.log(str2.indexOf("!"));
+console.log(str2.indexOf("!")); //returns 11
+
+var str3 = str2.slice(0,4);
+console.log(str3); 
+
+console.log(str2.includes("ell")); 
+//returns true
+
+let me = {name: "Edwin", age: 32}; 
+console.log(me.name); //returns Edwin
+console.log(me["name"]); //returns Edwin
+me.canDrink = "false";
+console.log(me);  
+// me.greet = () => `Hello my name is ${me.name}!`;
+// console.log(me.greet()); 
+me["greet"] = function () {
+    return `Hello my name is ${this.name}`;
+};
+console.log(me.greet()); 
+let myFirstName = me.name.split('');
+console.log(myFirstName); 
+if (myFirstName.includes("E", "e")){
+    console.log('Your name contains an \"e\"!');
+} else console.log('Your name does not contain an \"e\"'); 
